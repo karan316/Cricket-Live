a0aa853e50a978b35d6951e74c494226e9e72566
Karan Hejmadi <hejmadikaran9@gmail.com>
2020-09-23 19:56:45 +0530

    fixed map typerror, getting proper match response

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ interface Match {
     awayTeam: Team;
     scores: Score;
     venue: String;
-    status: String;
+    status?: String;
     matchSummaryText: String;
     isMatchAbandoned: Boolean;
     isMatchDrawn: Boolean;
@@ -30,36 +30,37 @@ export default function Home() {
     const [liveMatch, setLiveMatch] = useState<Match>(null);
     const [completedMatches, setCompletedMatches] = useState<Match[]>(null);
     const [upcomingMatches, setUpcomingMatches] = useState<Match[]>(null);
-    let completedIPLMatches: Match[] | undefined;
+    let completedIPLMatches: Match[];
     let upcomingIPLMatches: Match[];
     let liveIPLMatch: Match;
 
     const url =
         "https://dev132-cricket-live-scores-v1.p.rapidapi.com/matches.php";
-    const { data } = useSWR(
-        url,
-        async (url: string) =>
-            await axios({
-                url: url,
-                headers: {
-                    "content-type": "application/octet-stream",
-                    "x-rapidapi-host": process.env.NEXT_PUBLIC_RAPID_API_HOST,
-                    "x-rapidapi-key": process.env.NEXT_PUBLIC_RAPID_API_KEY,
-                    useQueryString: true,
-                },
-                params: {
-                    completedlimit: "5",
-                    inprogresslimit: "5",
-                    upcomingLimit: "5",
-                },
-            }).then((response: AxiosResponse) => response.data)
-    );
-
     try {
+        const { data } = useSWR(
+            url,
+            async (url: string) =>
+                await axios({
+                    url: url,
+                    headers: {
+                        "content-type": "application/octet-stream",
+                        "x-rapidapi-host":
+                            process.env.NEXT_PUBLIC_RAPID_API_HOST,
+                        "x-rapidapi-key": process.env.NEXT_PUBLIC_RAPID_API_KEY,
+                        useQueryString: true,
+                    },
+                    params: {
+                        completedlimit: "5",
+                        inprogresslimit: "5",
+                        upcomingLimit: "5",
+                    },
+                }).then((response: AxiosResponse) => response.data)
+        );
+        let matches = [];
         if (data) {
-            const {
+            ({
                 matchList: { matches },
-            } = data;
+            } = data);
 
             console.log("Matches: ", matches);
             {
@@ -127,42 +128,39 @@ export default function Home() {
 
             {
                 matches
-                    ? (liveIPLMatch = matches
-                          .find(
+                    ? (liveIPLMatch = (({
+                          name,
+                          homeTeam,
+                          awayTeam,
+                          isMatchDrawn,
+                          isMatchAbandoned,
+                          matchSummaryText,
+                          venue,
+                          scores,
+                      }) => ({
+                          name,
+                          homeTeam,
+                          awayTeam,
+                          isMatchDrawn,
+                          isMatchAbandoned,
+                          matchSummaryText,
+                          venue,
+                          scores,
+                      }))(
+                          matches.find(
                               (match) =>
                                   match.series.id === 2514 &&
                                   match.status === "LIVE"
                           )
-                          .map(
-                              ({
-                                  awayTeam,
-                                  name,
-                                  homeTeam,
-                                  isMatchDrawn,
-                                  isMatchAbandoned,
-                                  matchSummaryText,
-                                  venue,
-                                  scores,
-                              }) => ({
-                                  name,
-                                  homeTeam,
-                                  awayTeam,
-                                  isMatchDrawn,
-                                  isMatchAbandoned,
-                                  matchSummaryText,
-                                  venue,
-                                  scores,
-                              })
-                          ))
+                      ))
                     : (liveIPLMatch = null);
             }
-
             console.log("Completed: ", completedIPLMatches);
             console.log("LIVE: ", liveIPLMatch);
             console.log("Upcoming: ", upcomingIPLMatches);
         }
     } catch (error) {
-        console.log(error);
+        console.error(error);
     }
     // setCompletedMatches(completedIPLMatches);
     // setUpcomingMatches(upcomingIPLMatches);
